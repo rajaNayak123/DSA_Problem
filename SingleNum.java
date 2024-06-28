611716b212158f65a30b88e81cc2b98918295e58
rajaNayak123 <nayakraja151@gmail.com>
2024-06-28 20:24:32 +0530

    singleNumber

---
+++ SingleNum.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class SingleNum {
+    public static int findSingleNum(int arr[], int n) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            if (map.containsKey(arr[i])) {
+                map.put(arr[i], map.get(arr[i]) + 1);
+            } else {
+                map.put(arr[i], 1);
+            }
+        }
+        for(Map.Entry<Integer,Integer> val: map.entrySet()) {
+            if(val.getValue()==1){
+                return val.getKey();
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 1, 2, 2, 4, 1 };
+        int n = arr.length;
+        System.out.println(findSingleNum(arr, n));
+    }
+
+}
